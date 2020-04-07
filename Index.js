8d25c90c226fdbacbc952272dc22f420ce980e9e
GlassPaul <63299800+GlassPaul@users.noreply.github.com>
2020-04-07 14:41:56 +0100

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,26 @@
+let name; 
+let age;
+let jobRole = "Business Analyst"
+
+
+function persoanlData(name, age) {
+    console.log("Hello, I am " + name + " and I am " + age + " years old")
+    
+}
+
+persoanlData("Jacob", 44)
+persoanlData("Dean", 44)
+
+let numberOne;0
+let numberTwo;
+
+function addition(numberOne, numberTwo) {
+    let total = numberOne + numberTwo
+    console.log(total)
+
+}
+
+addition(50, 40)
+addition(30, 50)
+addition(300, 550)
+addition(330, 530)
